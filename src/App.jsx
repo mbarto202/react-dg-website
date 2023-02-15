ccd459ac0a34d2272e76cfcf3863fc03d7329cfc
mbarto202 <michael.d.barto@gmail.com>
2023-02-15 14:19:54 -0500

    components ready to use

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Title from './components/title/Title'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Library from './components/library/Library'
+import Games from './components/games/Games'
+import Footer from './components/footer/Footer'
+/**
+ *  @author Michael Barto
+ *  @date 2/13/2023
+ *  Mission Statement: The goal of child therapy is to help the child understand and manage their thoughts, feelings, and behaviors in a healthy way.
+ *  The therapist may use games, videos, or other creative activities to help the child express themselves.
+ *  My mission is to create a tool that helps guide therapists in reaching their goal.
+ */
+
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Title />
+            <Nav />
+            <About />
+            <Library />
+            <Games />
+            <Footer />
+        </>
+    )
+}
+
+export default App
